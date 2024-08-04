429658fa0585e5daec19f1ff23b7393549585fa9
cwadven <cwadven@naver.com>
2024-08-04 14:05:01 +0900

    Feat: Add help_text
    - Add help_text

--- map/forms/admin_forms.py
+++ map/forms/admin_forms.py
@@ -4,7 +4,11 @@ from map.models import Map
 
 
 class MapAdminForm(PreSignedUrlAdminForm):
-    main_image_file = forms.ImageField(required=False)
+    main_image_file = forms.ImageField(
+        label='메인 이미지 업로드 하기',
+        help_text='이미지를 업로드 후, 저장하면 URL이 자동으로 입력됩니다.',
+        required=False,
+    )
 
     class Meta:
         model = Map
